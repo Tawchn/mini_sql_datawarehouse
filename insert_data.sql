2b1903f87cb52ec2294395f15f8947531b765d33
Tawanda <tawandamucheriwa@gmail.com>
2026-01-07 16:27:29 +0000

    Add sample council-style payment data

---
+++ insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO bronze.bcc_payments_sample
+VALUES
+('Housing', 'ABC Maintenance Ltd', 'Repairs', 1250.00, '2025-01-05'),
+('IT Services', 'CloudNet UK', 'Software', 2200.00, '2025-01-08'),
+('Education', 'BrightFuture Supplies', 'Stationery', 540.50, '2025-01-15');
+
